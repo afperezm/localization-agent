540d98b62cda2b5f550184931bc0bd9ed408c79c
Andrés Pérez <afperesm@unal.edu.co>
2015-10-23 02:49:28 -0500

    Add parallel environment for testing

--- locagent/GameEnvironment.lua
+++ locagent/GameEnvironment.lua
@@ -8,6 +8,7 @@ local gameEnv = torch.class('locagent.GameEnvironment')
 ]]
 function gameEnv:__init(_opt)
   local _opt = _opt or {}
+  self._isTraining = true
   self._verbose = _opt.verbose or 0
   self._state = {reward = -1}
   self:_init(_opt.env, _opt.env_params, _opt.config_file)
@@ -35,19 +36,7 @@ function gameEnv:_init(_env, _params, _config_file)
   py.exec([[from detection.boxsearch.BoxSearchTask import BoxSearchTask]])
   py.exec([[import detection.boxsearch.BoxSearchState as BoxSearchState]])
 
-  self._actions   = self:getActions()
-
-  py.exec([[k = 0]])
-
-  py.exec([[mode = 'train']])
-
-  py.exec([[maxInteractions = config.geti(mode + 'Interactions')]])
-
-  py.exec([[imageList = config.get(mode + 'Database')]])
-
-  py.exec([[groundTruthFile = config.get(mode + 'GroundTruth')]])
-
-  py.exec([[controller = DotMap()]])
+  self._actions = self:getActions()
 
 py.exec([=[
 def prepareImage(image):
@@ -61,38 +50,60 @@ def coverRegion(box):
   h = box[3]-box[1]
   b1 = map(int, [box[0] + w*0.5 - w*config.getf('markWidth'), box[1], box[0] + w*0.5 + w*config.getf('markWidth'), box[3]])
   b2 = map(int, [box[0], box[1] + h*0.5 - h*config.getf('markWidth'), box[2], box[1] + h*0.5 + h*config.getf('markWidth')])
-  draw = ImageDraw.Draw(task.env.state.visibleImage)
+  if isTraining:
+    draw = ImageDraw.Draw(task.env.state.visibleImage)
+  else:
+    draw = ImageDraw.Draw(testingTask.env.state.visibleImage)
   draw.rectangle(b1, fill=1)
   draw.rectangle(b2, fill=1)
   del draw
-]=])
+]=], {isTraining = self._isTraining})
 
+  print('\nInitializing training environment')
+  py.exec([[k = 0]])
+  py.exec([[maxInteractions = config.geti('trainInteractions')]])
+  py.exec([[imageList = config.get('trainDatabase')]])
+  py.exec([[groundTruthFile = config.get('trainGroundTruth')]])
+  py.exec([[controller = DotMap()]])
   py.exec([[controller.net.coverRegion = coverRegion]])
   py.exec([[controller.net.prepareImage = prepareImage]])
-
-  py.exec([[environment = BoxSearchEnvironment(imageList, mode, controller, groundTruthFile)]])
-
+  py.exec([[environment = BoxSearchEnvironment(imageList, 'train', controller, groundTruthFile)]])
   py.exec([[task = BoxSearchTask(environment, groundTruthFile)]])
+
+  print('\nInitializing testing environment')
+  py.exec([[testingK = 0]])
+  py.exec([[testingMaxInteractions = config.geti('testInteractions')]])
+  py.exec([[testingImageList = config.get('testDatabase')]])
+  py.exec([[testingGroundTruthFile = config.get('testGroundTruth')]])
+  py.exec([[testingController = DotMap()]])
+  py.exec([[testingController.net.coverRegion = coverRegion]])
+  py.exec([[testingController.net.prepareImage = prepareImage]])
+  py.exec([[testingEnvironment = BoxSearchEnvironment(testingImageList, 'test', testingController, testingGroundTruthFile)]])
+  py.exec([[testingTask = BoxSearchTask(testingEnvironment, testingGroundTruthFile)]])
 
   return self
 end
 
 
---[[ Starts a new game and returns its state.
+--[[ Starts a new game by loading a new episode and returns its state.
 ]]
-function gameEnv:newGame()
+function gameEnv:newGame(isTraining)
 
-  py.exec([[k = 0]])
-
-  py.exec([[task.env.loadNextEpisode()]])
-
---  if training then
---    py.exec([[mode = 'train']])
---    py.exec([[maxInteractions = config.geti('trainInteractions')]])
---  else
---    py.exec([[mode = 'test']])
---    py.exec([[maxInteractions = config.geti('testInteractions')]])
---  end
+  if isTraining then
+    self._isTraining = true
+    -- Load the next episode in the training game
+    if py.eval([[k]]) > 0 then
+      py.exec([[k = 0]])
+      py.exec([[task.env.loadNextEpisode()]])
+    end
+  else
+    self._isTraining = false
+    -- Load the next episode in the testing game
+    if py.eval([[testingK]]) > 0 then
+      py.exec([[testingK = 0]])
+      py.exec([[testingTask.env.loadNextEpisode()]])
+    end
+  end
 
   return self:getState()
 end
@@ -100,8 +111,8 @@ end
 
 --[[ Starts a new game by loading a new episode and returns its state.
 ]]
-function gameEnv:nextRandomGame(k)
-  return self:newGame()
+function gameEnv:nextRandomGame(isTraining)
+  return self:newGame(isTraining)
 end
 
 
@@ -111,11 +122,17 @@ telling whether the game has finished.
 ]]
 function gameEnv:getState()
 
-  py.exec([[sensors = task.env.getSensors()]])
-  py.exec([[cropped_image = task.env.state.visibleImage.crop(map(int,sensors['state'])).resize([50,50])]])
-
-  self._state.observation = py.eval([[numpy.array(cropped_image.getdata()).reshape(cropped_image.size[0], cropped_image.size[1], 3)]])
-  self._state.terminal = py.eval([[task.env.episodeDone or k >= maxInteractions]])
+  if self._isTraining then
+    py.exec([[sensors = task.env.getSensors()]])
+    py.exec([[cropped_image = task.env.state.visibleImage.crop(map(int,sensors['state'])).resize([50,50])]])
+    self._state.observation = py.eval([[numpy.array(cropped_image.getdata()).reshape(cropped_image.size[0], cropped_image.size[1], 3)]])
+    self._state.terminal = py.eval([[task.env.episodeDone or k >= maxInteractions]])
+  else
+    py.exec([[sensors = testingTask.env.getSensors()]])
+    py.exec([[cropped_image = testingTask.env.state.visibleImage.crop(map(int,sensors['state'])).resize([50,50])]])
+    self._state.observation = py.eval([[numpy.array(cropped_image.getdata()).reshape(cropped_image.size[0], cropped_image.size[1], 3)]])
+    self._state.terminal = py.eval([[testingTask.env.episodeDone or testingK >= testingMaxInteractions]])
+  end
 
   return self._state.observation, self._state.reward, self._state.terminal
 end
@@ -123,14 +140,18 @@ end
 
 --[[ Plays a given action in the game and returns the game state.
 ]]
-function gameEnv:step(action, training)
+function gameEnv:step(action)
   assert(action > 0)
 
-  py.exec([[task.performAction([actionChosen, float(actionValue)])]], {actionChosen = self._actions[action], actionValue = -1})
-
-  self._state.reward = py.eval([[task.getReward()]])
-
-  py.exec([[k += 1]])
+  if self._isTraining then
+    py.exec([[task.performAction([actionChosen, float(actionValue)])]], {actionChosen = self._actions[action], actionValue = -1})
+    self._state.reward = py.eval([[task.getReward()]])
+    py.exec([[k += 1]])
+  else
+    py.exec([[testingTask.performAction([actionChosen, float(actionValue)])]], {actionChosen = self._actions[action], actionValue = -1})
+    self._state.reward = py.eval([[testingTask.getReward()]])
+    py.exec([[testingK += 1]])
+  end
 
   return self:getState()
 end
