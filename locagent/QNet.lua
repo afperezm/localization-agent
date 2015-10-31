ce4f6c1c2cf325defefa05e5c189f1740d77a982
Andrés Pérez <afperesm@unal.edu.co>
2015-10-30 19:21:44 -0500

    Create function to load Q-Network

--- locagent/QNet.lua
+++ locagent/QNet.lua
@@ -2,27 +2,32 @@
 
 require 'nn'
 
-local model = nn.Sequential()
+function load_qnet(args)
 
--- fc1
-model:add(nn.Linear(4196, 1024))
-model.modules[#model.modules].weight:normal(0, 0.005)
-model.modules[#model.modules].bias:fill(0.1)
+  local model = nn.Sequential()
 
--- relu1
-model:add(nn.ReLU(true))
+  -- fc1
+  --model:add(nn.Linear(4196, 1024))
+  model:add(nn.Linear(4096, 1024))
+  model.modules[#model.modules].weight:normal(0, 0.005)
+  model.modules[#model.modules].bias:fill(0.1)
 
--- fc2
-model:add(nn.Linear(1024, 1024))
-model.modules[#model.modules].weight:normal(0, 0.005)
-model.modules[#model.modules].bias:fill(0.1)
+  -- relu1
+  model:add(nn.ReLU(true))
 
--- relu2
-model:add(nn.ReLU(true))
+  -- fc2
+  model:add(nn.Linear(1024, 1024))
+  model.modules[#model.modules].weight:normal(0, 0.005)
+  model.modules[#model.modules].bias:fill(0.1)
 
--- q-values
-model:add(nn.Linear(1024, 10))
-model.modules[#model.modules].weight:normal(0, 0.01)
-model.modules[#model.modules].bias:fill(0)
+  -- relu2
+  model:add(nn.ReLU(true))
 
-return model
+  -- q-values
+  model:add(nn.Linear(1024, 10))
+  model.modules[#model.modules].weight:normal(0, 0.01)
+  model.modules[#model.modules].bias:fill(0)
+
+  return model
+
+end
