8910b6ca0da5fce3345a575cd824433701bc99fd
Andrés Pérez <afperesm@unal.edu.co>
2015-10-30 19:23:37 -0500

    Merge AlexNet and QNet

--- locagent/convnet_jointnet.lua
+++ locagent/convnet_jointnet.lua
@@ -1,6 +1,41 @@
-local alexnet = require 'AlexNet'
-local qnet = require 'QNet'
+require 'nn'
+require 'AlexNet'
+require 'QNet'
 
-local model = nn.Sequential()
+return function(args)
 
-return model
+    local add_history = args.add_history or true
+    local prototxt = args.prototxt or '/home/jccaicedoru/bvlc_alexnet/deploy.prototxt'
+    local binary = args.binary or '/home/jccaicedoru/bvlc_alexnet/bvlc_alexnet.caffemodel'
+
+    local alexnet = load_alexnet(args)
+    local qnet = load_qnet(args)
+
+    -- Remove soft max layer
+    alexnet:remove(alexnet:size())
+
+    -- Remove output layer
+    alexnet:remove(alexnet:size())
+
+    -- Remove fc7 layer
+    alexnet:remove(alexnet:size())
+    alexnet:remove(alexnet:size())
+    alexnet:remove(alexnet:size())
+
+    if add_history then
+      local mlp = nn.Concat(1)
+      local input_size = qnet:get(1).weight:size()[2]
+      mlp:add(nn.Linear(input_size, 100))
+      mlp:add(qnet:get(1))
+    else
+      alexnet:add(qnet:get(1))
+    end
+
+    alexnet:add(qnet:get(2))
+
+    alexnet:add(qnet:get(3))
+
+    alexnet:add(qnet:get(4))
+
+    return alexnet
+end
