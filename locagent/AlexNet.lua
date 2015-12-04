b994ca2add1f534bec74c35305944cbd8d1e4c99
Andrés Pérez <afperesm@unal.edu.co>
2015-12-04 00:14:31 -0500

    Use cudnn to load caffe network

--- locagent/AlexNet.lua
+++ locagent/AlexNet.lua
@@ -1,5 +1,5 @@
 require 'loadcaffe'
 
 function load_alexnet(caffe_prototxt, caffe_binary)
-  return loadcaffe.load(caffe_prototxt, caffe_binary)
+  return loadcaffe.load(caffe_prototxt, caffe_binary, 'cudnn')
 end
